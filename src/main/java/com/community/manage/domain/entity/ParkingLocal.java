f3b537ba707bb8bdeac0e253ea5b39d0cc9937c7
XiaoG123 <598753711@qq.com>
2020-07-11 16:28:59 +0800

    test

---
+++ src/main/java/com/community/manage/domain/entity/ParkingLocal.java
@@ -0,0 +1,14 @@
+package com.community.manage.domain.entity;
+
+import lombok.Data;
+
+@Data
+public class ParkingLocal {
+//    查询的关键字 主键id,小区,停车的编号
+    private int localId;
+    private String localCommunity;
+    private String localNumber;
+
+//    关联的信息
+
+}
