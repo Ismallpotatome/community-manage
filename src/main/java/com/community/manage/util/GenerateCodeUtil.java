9a2666c4c80e3e4b9099070362be1aed70f82459
long <danny98ljt@foxmail.com>
2020-07-11 15:11:53 +0800

    龙嘉涛2020.7.11 15:11

---
+++ src/main/java/com/community/manage/util/GenerateCodeUtil.java
@@ -0,0 +1,22 @@
+package com.community.manage.util;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class GenerateCodeUtil {
+
+    /**
+     * 生成订单号
+     */
+    // 用于生成订单标示，自增
+    public static int index = 100;
+    public static synchronized String createCodeNum(String pre) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("YYMMddHHmmss");
+        String orderNum = pre + dateFormat.format(new Date());
+        orderNum += index++;
+        if (index > 999){
+            index = 100;
+        }
+        return orderNum;
+    }
+}
