37cc6f32c9e3882d97bd5f599dbb54658f8021c4
w'n'y'y'z <809054082@qq.com>
2020-07-14 19:48:14 +0800

    "业主模块的ServiceImpl" ----陈泳霖 2020.7.14

---
+++ src/main/java/com/community/manage/mapper/owner/OwnerVehicleMapper.java
@@ -0,0 +1,14 @@
+package com.community.manage.mapper.owner;
+
+import com.community.manage.domain.dto.SearchDto;
+import com.community.manage.domain.dto.VehicleDto;
+import com.community.manage.domain.entity.Vehicle;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+
+public interface OwnerVehicleMapper {
+
+    //根据关键字和时间进行分页查询
+    List<VehicleDto> selectBySearchAndLimit(@Param("searchDto")SearchDto searchDto);
+}
