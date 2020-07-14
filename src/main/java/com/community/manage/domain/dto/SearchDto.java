ae0928342686319bb15ec97b0b00bb379d219c50
w'n'y'y'z <809054082@qq.com>
2020-07-14 19:21:28 +0800

    封装的查询信息类 ----陈泳霖 2020.7.14

---
+++ src/main/java/com/community/manage/domain/dto/SearchDto.java
@@ -0,0 +1,14 @@
+package com.community.manage.domain.dto;
+
+import lombok.Data;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Data
+//"分页搜索"公共类
+public class SearchDto {
+    private String keyword;
+    private String startDate;
+    private String endData;
+    private int page;
+    private int size;
+}
