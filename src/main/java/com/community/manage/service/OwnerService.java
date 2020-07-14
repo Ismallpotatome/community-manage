f87b671ad9fc81c149dd6b767c486ea6a0ea1127
w'n'y'y'z <809054082@qq.com>
2020-07-14 19:36:57 +0800

    "业主模块的Service" ----陈泳霖 2020.7.14

---
+++ src/main/java/com/community/manage/service/OwnerService.java
@@ -0,0 +1,15 @@
+package com.community.manage.service;
+
+import com.community.manage.domain.dto.SearchDto;
+import com.community.manage.domain.dto.VehicleDto;
+import com.community.manage.domain.entity.Owner;
+import com.community.manage.domain.entity.Vehicle;
+
+import java.util.List;
+
+public interface OwnerService {
+    //业主人员信息的关键字分页查询
+    List<Owner> personnelSearchByPage(SearchDto searchDto);
+    //业主车辆信息的关键字分页查询
+    List<VehicleDto> vehicleSearchByPage(SearchDto searchDto);
+}
