2194606699240dab5980ac3c0e6ffade6313f452
w'n'y'y'z <809054082@qq.com>
2020-07-15 09:36:05 +0800

    "车辆信息的Mapper" ----陈泳霖 2020.7.14

--- src/main/java/com/community/manage/mapper/owner/OwnerVehicleMapper.java
+++ src/main/java/com/community/manage/mapper/owner/OwnerVehicleMapper.java
@@ -15,5 +15,7 @@ public interface OwnerVehicleMapper {
     int insertVehicleDto(@Param("vehicle") Vehicle vehicle);
     //根据id批量修改数据状态
     int updateBatchVehicle(@Param("vehicleIdList") List<Integer> vehicleIdList);
+    //根据id单个修改数据状态
+    int updateSingleVehicle(@Param("vehicleId") Integer vehicleId);
 
 }
