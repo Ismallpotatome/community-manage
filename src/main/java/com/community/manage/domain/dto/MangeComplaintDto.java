ecc9895aa91b9dd4e32dadec44fb3d537b3a355e
XiaoG123 <598753711@qq.com>
2020-07-16 11:28:30 +0800

    梁岫祥 2020-7-16 11:27 完成了保修管理的模块

---
+++ src/main/java/com/community/manage/domain/dto/MangeComplaintDto.java
@@ -0,0 +1,10 @@
+package com.community.manage.domain.dto;
+
+import com.community.manage.domain.entity.TbMangeComplaint;
+import lombok.Data;
+
+import java.io.Serializable;
+
+@Data
+public class MangeComplaintDto extends TbMangeComplaint implements Serializable {
+}
