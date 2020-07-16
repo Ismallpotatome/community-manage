8ce70c804f3f125631017556f27188672c5f1443
XiaoG123 <598753711@qq.com>
2020-07-16 12:01:32 +0800

    梁岫祥 2020-7-16 12:01 完成了邮箱管理的模块

---
+++ src/main/java/com/community/manage/controller/MangeMailController.java
@@ -0,0 +1,55 @@
+package com.community.manage.controller;
+
+import com.community.manage.domain.dto.MangeActivityDto;
+import com.community.manage.domain.dto.MangeMailDto;
+import com.community.manage.domain.dto.SearchsDto;
+import com.community.manage.domain.entity.TbMangeActivity;
+import com.community.manage.domain.entity.TbMangeMail;
+import com.community.manage.service.MangeMailService;
+import com.community.manage.util.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+@RequestMapping("mail")
+@RestController
+public class MangeMailController {
+    @Resource
+    MangeMailService mangeMailService;
+
+    @PostMapping("select")
+    public ResponseEntity<List<TbMangeMail>> selectAll(@RequestBody SearchsDto searchsDto, @RequestParam(defaultValue = "1") int limit, @RequestParam(defaultValue = "10") int offset){
+
+
+        List<TbMangeMail> tbMangeMails = mangeMailService.selectAll(searchsDto, limit, offset);
+        return ResponseEntity.success(tbMangeMails);
+    }
+
+    //添加一条信息
+    @PostMapping("insert")
+    public ResponseEntity insert(@RequestBody MangeMailDto mangeMailDto){
+        ResponseEntity insert = mangeMailService.insert(mangeMailDto);
+
+        return insert;
+    }
+
+    //修改所有信息
+    @PostMapping("updateAll")
+    public ResponseEntity updateAll(@RequestBody MangeMailDto mangeMailDto){
+
+        ResponseEntity responseEntity = mangeMailService.updateAll(mangeMailDto);
+
+        return responseEntity;
+    }
+
+    //批量删除信息
+    @PostMapping("del")
+    public ResponseEntity delOne(@RequestBody List<MangeMailDto> mangeMailDto){
+
+        ResponseEntity del = mangeMailService.del(mangeMailDto);
+
+        return del;
+
+    }
+}
